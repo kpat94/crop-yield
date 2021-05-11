2d03b9eb48e121b9e6ae8906f1c495760e18fcba
kpat94 <parth@trustprovenance.com>
2021-05-11 18:10:15 +0930

    Tidying up. Transform test data. Randomly split data into training and validation sets. Training loop function. Hyperparameter tuning using ray tune.

--- src/main.py
+++ src/main.py
@@ -25,29 +25,20 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 #%%
 def load_data():
        pesticides_frame = pd.read_csv("Inputs_Pesticides_Use_E_All_Data_NOFLAG.csv", engine='python')
-       print("***** Pesticides \n")
        pesticides_frame.head
-
-       
+  
        # Read fertilizer data in the form of products
        fertilizers_frame = pd.read_csv("Inputs_FertilizersProduct_E_All_Data_NOFLAG.csv", engine='python')
-       print("***** Fertilizer products \n")
        fertilizers_frame.head
        
-
        # Read the crop yield data
        yield_frame = pd.read_csv("Production_Crops_E_All_Data_NOFLAG.csv",engine='python')
-       print("***** Production yield \n")
        yield_frame.head
-
 
        # %<br>
        # Get the fertilizer usage in terms of agriculture
 
-       # In[4]:
-
-
-       
+       # In[4]:     
        print("Fertilizer products",fertilizers_frame.columns)
        print("\n")
        print("Input pesticides", pesticides_frame.columns)
@@ -55,20 +46,12 @@ def load_data():
        # Filter out the data for Australia
        is_Country_Australia_Products = fertilizers_frame['Area']=="Australia"
        Australia_Fertilizers_Products = fertilizers_frame[is_Country_Australia_Products]
-
-
        # In[5]:
-
-
        is_Country_Australia_Pesticides = pesticides_frame['Area']=="Australia"
        Australia_Pesticides = pesticides_frame[is_Country_Australia_Pesticides]
 
-
        # Get the fertilizer usage for agricultural use
-
        # In[6]:
-
-
        is_Agricultural = Australia_Fertilizers_Products['Element']=="Agricultural Use"
        Australia_Fertilizers_Products_Agricultural = Australia_Fertilizers_Products[is_Agricultural]
        print("Fertilizers \n")
@@ -78,16 +61,12 @@ def load_data():
        print("Pesticides \n")
        print(Australia_Pesticides.columns)
        print(Australia_Pesticides['Item'])
-
-
+       
        # %<br>
        # 7 years for training - 2007 to 2013<br>
        # 3 years fro validation - 2014 to 2016<br>
        # 2017 for testing
-
        # In[7]:
-
-
        X_ten_years_fertlizers = Australia_Fertilizers_Products_Agricultural.drop(['Area Code', 'Area', 'Item Code', 'Element Code','Element',
               'Unit','Y2002', 'Y2003', 'Y2004', 'Y2005', 'Y2006', 'Y2017'], axis=1)
        print(X_ten_years_fertlizers)
@@ -96,8 +75,6 @@ def load_data():
        print("2017 \n",fertilizer_2017)
 
        # In[8]:
-
-
        X_ten_years_pesticides = Australia_Pesticides.drop(['Area Code', 'Area', 'Item Code', 'Element Code', 'Element',
               'Unit', 'Y1990', 'Y1991', 'Y1992', 'Y1993', 'Y1994', 'Y1995', 'Y1996', 'Y1997', 'Y1998', 'Y1999', 'Y2000', 'Y2001', 'Y2002', 'Y2003', 'Y2004', 'Y2005', 'Y2006', 'Y2017'], axis=1)
        print(X_ten_years_pesticides)
@@ -107,24 +84,17 @@ def load_data():
 
        # %<br>
        # Drop Ammonia anyhydrous and Ammonia nitrate from 2007 to 2013. It has not been used in these years.
-
        # In[9]:
-
        X_ten_years_fertlizers = X_ten_years_fertlizers.drop([602, 607], axis=0)
        print(X_ten_years_fertlizers)
        print(X_ten_years_fertlizers.columns)
        fertilizer_2017 = fertilizer_2017.drop([602, 607], axis=0)
        print(fertilizer_2017)
-
-
-
+       
        # %<br>
        # 2009 to 2013
        # ## Handle missing values from 2009 to 2013
-
        # In[10]:
-
-
        interpolate_2009_to_2016 = X_ten_years_fertlizers.interpolate()
        interpolate_2009_to_2016
        interpolate_2017 = fertilizer_2017.interpolate()
@@ -133,14 +103,12 @@ def load_data():
        # interpolate_pesticides_2017 = pesticides_2017.interpolate()
        # print("interpolated 2017 pesticides \n",interpolate_pesticides_2017)
 
-
        # %<br>
        #  Interpolate missing fertlizers data<br>
        #  Replace na with mean values of 2004,2005,2009,2010
        # ##  Replace 2007 nan values with the mean of 2004 and 2005
 
        # In[11]:
-
 
        interpolate_2004_2005 = Australia_Fertilizers_Products_Agricultural[['Y2004','Y2005']].copy().interpolate()
        index_names = interpolate_2004_2005.index
@@ -154,28 +122,17 @@ def load_data():
        print("Mean of 2015 and 2016 === \n")
        print(pesticides_2015_2016)
 
-
        # Use this for 2007
 
        # In[ ]:
-
-
-
-
-
        # ##  Replace 2008 nan values with the mean of 2009 and 2010
 
        # In[12]:
-
-
        interpolate_2009_2010 = Australia_Fertilizers_Products_Agricultural[['Y2009','Y2010']].copy().interpolate()
        index_names = interpolate_2009_2010.index
        index_names
 
-
        # In[13]:
-
-
        interpolate_2009_2010['mean'] = interpolate_2009_2010.mean(axis=1)
        print("Mean of 2009 and 2010 ==== \n")
        # Use this for 2008
@@ -184,10 +141,7 @@ def load_data():
 
 
        # ## Populate 2007 nan values with computed mean
-
        # In[14]:
-
-
        # Fertilizers
        i=0
        for i in range(1,16):
@@ -198,37 +152,24 @@ def load_data():
        for j in range(1,7):
               pesticides_2017.iloc[0:j,] = pesticides_2015_2016['mean'].iloc[0:j]
 
-       print(pesticides_2017)
-
        # ## Populate 2008 nan values with computed mean
-
        # In[15]:
-
-
        j=0
        for j in range(1,16):
               interpolate_2009_to_2016.iloc[0:j,2:3] = interpolate_2009_2010['mean'].iloc[0:j]
-              
-       
+                
        interpolate_2009_to_2016.iloc[15, 1] = 1437658.50
        print(interpolate_2009_to_2016.iloc[15, 1])
        interpolate_2009_to_2016.iloc[15, 2] = 171351.00
 
-
        # In[16]:
-
-
        X_ten_years_fertilizers_interpolated = interpolate_2009_to_2016
        X_ten_years_fertilizers_interpolated
        fertilizer_2017_interpolated = interpolate_2017
        print(fertilizer_2017_interpolated)
 
-
        # #### TODO: Combine pesticdes and fertilizers data
-
        # In[17]:
-
-
        print(X_ten_years_pesticides)
        print(X_ten_years_fertilizers_interpolated)
 
@@ -252,19 +193,13 @@ def load_data():
        fertilizer_2017_tensor = torch.from_numpy(fertilizer_2017_wo_item.values)
        print("\t Fertilizers tensor", fertilizers_tensor)
 
-
-
        # In[18]:
-
-
        print("Train : \n",pesticides_tensor.shape)
        print("Train: \n",fertilizers_tensor.shape)
        print("Test: \n",pesticides_2017_tensor.shape)
        print("Test: \n",fertilizer_2017_tensor.shape)
 
        # TODO: Normalize inputs before concatenation
-
-
        pesticides_and_fertilizers = torch.cat((pesticides_tensor, fertilizers_tensor), dim=0)
        print(pesticides_and_fertilizers.shape)
        print("\n")
@@ -283,8 +218,6 @@ def load_data():
        # #### TODO: Create output vector
 
        # In[19]:
-
-
        print(yield_frame.columns)
        #%%
        yield_frame.head
@@ -321,10 +254,7 @@ def load_data():
        print("\n")
        print("Number of unique crops from 2007 to 2016",len(Australia_yield_2007_to_2016['Item'].unique()))
 
-
        # In[20]:
-
-
        yield_without_Item = Australia_yield_2007_to_2016.copy().drop(['Item'],axis=1)
        yield_without_Item_2017 = Australia_yield_2017.copy().drop(['Item'],axis=1)
 
@@ -336,7 +266,6 @@ def load_data():
        yield_tensor_2017 = torch.from_numpy(yield_without_Item_2017.values)
        print("Shape: ", yield_tensor_2017.shape)
        print("\t Yield tensor", yield_tensor_2017)
-
 
        # Training + Validation: Output
        Y_train_ten = yield_tensor
@@ -358,30 +287,26 @@ test_output = load_data()[3]
 
 x = training_input
 y = training_ouput
-print("X --> \n",x)
-print("\n")
-print(x.shape)
-print("Y --> \n",y)
-print("\n")
-print(y.shape)
-
+#%%
 
 # Transforms
 
-
+# Training data
 x_tensor = torch.Tensor(x.float())
 x_data = (x - x.mean())/(x.max() - x.min())
 test = torch.Tensor([[0,4,5]])
-print(x.shape)
 x_tensor_normalized = x.normal_()
-print("Normal ", x_tensor_normalized)
-
-
-# Rescale the output to match the range of the input
 y_data = (y.normal_())
-print("Rescaled y values: ",y_data)
-#%%
 
+#%%
+# Test data
+transformed_test_x = torch.Tensor(test_input.float())
+transformed_test_x = transformed_test_x.normal_()
+transformed_test_y = torch.Tensor(test_output.float())
+transformed_test_y = transformed_test_y.normal_()
+#%%
+#%%
+# Define model
 class NeuralNet(nn.Module):
        def __init__(self, D_in, H1, H2, H3, D_out):
               super(NeuralNet, self).__init__()
@@ -390,38 +315,11 @@ class NeuralNet(nn.Module):
               self.linear3 = nn.Linear(H2, H3)
               self.linear4 = nn.Linear(H3, D_out)
        def forward(self, x):
-              print("Forward pass")
               y_pred = self.linear1(x)
               y_pred = self.linear2(y_pred)
               y_pred = self.linear3(y_pred)
               y_pred = self.linear4(y_pred)
               return y_pred
-
-
-# In[24]:
-
-
-H1, H2, H3 = 500, 1000, 200
-D_in, D_out = 22, 101
-
-loss_fn = torch.nn.MSELoss(reduction='sum')
-learning_rate = 0.00001
-
-model1 = NeuralNet(D_in, H1, H2, H3, D_out)
-
-optimizer = torch.optim.SGD(model1.parameters(), lr=learning_rate)
-
-losses1 = []
-#%%
-for t in range(50):
-       y_pred = model1(x)
-       loss = loss_fn(y_pred, y)
-       print(t, loss.item())
-       losses1.append(loss.item())
-       optimizer.zero_grad()
-       loss.backward()
-       optimizer.step()       
-#%%
 
 # In[ ]:
 
@@ -436,57 +334,60 @@ import torch.optim as optim
 from torch.utils.data import random_split
 import torchvision
 import torchvision.transforms as transforms
-# from ray import tune
-# from ray.tune import CLIReporter
-# from ray.tune.schedulers import ASHAScheduler
+from random import randint
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler
 
 # In[73]:
 
 
 def get_accuracy(model, predicted, actual, threshold_percentage):
     num_items = len(actual)
-    print("Length: ",num_items)
     X = predicted.view(num_items)
-    print(X)
-    print(X.shape)
     Y = actual.view(num_items)
-    print(Y)
-    print(Y.shape)
     num_correct = torch.sum(torch.abs(X-Y)<torch.abs(threshold_percentage*Y))
-    print(f"No of correct predictions: {num_correct}")
     accuracy = (num_correct.item()*100.0/num_items)
-    print(f"{accuracy}")
+    return (num_correct, accuracy)
     
 
-
-# # Using this loop
+def random_split_training(trainset_x, trainset_y):
+       # TODO:
+       # Select 2 random numbers between 1 and 10
+       # Use them to index into x and y.
+       # That will give us the training and validation set
+       # Rest of the indices are thus our training set
+       index1 = randint(0,9)
+       index2 = randint(0,9)
+       indices=[]
+       i=0
+       for i in range(0,10):
+              if(i not in [index1,index2]):
+                 indices.append(i)    
+       
+       indices = torch.tensor(indices)
+       val_indices = torch.tensor([index1, index2])
+       t_subset_x = torch.index_select(trainset_x, 1, indices)
+       t_subset_y = torch.index_select(trainset_y, 1, indices)
+       v_subset_x = torch.index_select(trainset_x, 1,val_indices)
+       v_subset_y = torch.index_select(trainset_y, 1, val_indices)
+       return (t_subset_x, t_subset_y, v_subset_x, v_subset_y)
 # 
 
 # In[76]:
 
-
 # loss_fn = torch.nn.MSELoss(reduction='sum')
 loss_fn = torch.nn.MSELoss()
-
 losses2 = []
-
 H1, H2, H3 = 500, 1000, 200
-
 D_in, D_out = 220, 1010
-
 model2 = NeuralNet(D_in, H1, H2, H3, D_out)
-
 optimizer = torch.optim.SGD(model2.parameters(), lr=1e-4)
 
-# print(x)
-# print(x.shape)
 transformed_x = torch.reshape(x_tensor_normalized,(1,220))
 transformed_y = y_data
-print(transformed_x)
-print(transformed_x.shape)
 y = torch.reshape(y, (1,1010))
-print(y)
-print(y.shape)
+
 #%%
 # print(y.shape)
 
@@ -502,84 +403,109 @@ for t in range(3):
        optimizer.zero_grad()
        loss.backward()
        
+#%%     
+# Function to train and validate data. Save the best model based on validation loss
+# 
+# params:
+# config: hyperparameter search space
+# 
 def train_crop_yield(config):
-       net = NeuralNet(D_in, config['H1'], config['H2'], config['H3'], D_out)
+       net = NeuralNet(176, config['H1'], config['H2'], config['H3'], 8)
        criterion = torch.nn.MSELoss()
        optimizer = torch.optim.SGD(net.parameters(), lr=config["lr"], momentum=0.9)
-       train_set = torch.cat(transformed_x, transformed_y, dim=0)
-       test_set = torch.cat(test_input, test_output, dim=0)
-
-print(losses2)
-print("Predicted -> ",y_pred)
-loss_fn_test = torch.nn.MSELoss()
-print("Predicted loss ->", loss_fn_test(y_pred.reshape(1010,1), transformed_y.reshape(1010,1).float()))
-# Make the forward pass and then find accuracy of predictions. Alternative: Pass model to below function
-get_accuracy(model2, y_pred.reshape(1010,1), transformed_y.reshape(1010,1), 0.10)
-
-#%%
-# train_set = torch.cat((transformed_x.reshape(22,10), transformed_y.reshape(10,101)),)
-# test_set = torch.cat(test_input, test_output, dim=0)
-print(transformed_x.reshape(22,10).shape)
-print(transformed_y.reshape(10,101).shape)
-# print(train_set)
-x_by_years = transformed_x.reshape(22,10)
-y_by_years = transformed_y.reshape(101,10)
-print(x_by_years)
-print(y_by_years)
-#%%
-# TODO:
-# Select 2 random numbers between 1 and 10
-# Use them to index into x and y.
-# That will give us the validation set
-# Rest of the indices are thus our training set
-
-# In[71]:
-
-
-# %%<br>
-# %%<br>
-# Save the model that has the best accuracy
-
-# In[ ]:
-
-# Save the best model
-
-torch.save(model4.state_dict(), './fcn_trained.dat')
-# Load the trained parameters
-model2.load_state_dict(torch.load('./fcn_trained.dat'))
-# Set the model to evaluation mode
-model2.eval()
-#%%
-def predict(input_values):
+       # train_set = torch.cat((transformed_x, transformed_y), dim=0)
+       # test_set = torch.cat((test_input, test_output), dim=0)
+       
+       x_by_years = transformed_x.reshape(22,10)
+       y_by_years = transformed_y.reshape(101,10)
+       
+       train_subset_x, train_subset_y, val_subset_x, val_subset_y = random_split_training(x_by_years, y_by_years)
+       
+       for epoch in range(100):
+              running_loss = 0.0
+              epoch_steps = 0
+              # Zero the accumulated gradients
+              optimizer.zero_grad()
+              # forward + backward + optimize
+              output = net(train_subset_x.float().reshape(1,176))
+              loss = criterion(output, train_subset_y.float())
+              loss.backward()
+              optimizer.step()
+              # print statistics
+              running_loss += loss.item()
+              epoch_steps += 1
+              if epoch%5==4:
+                     print("[%d] loss: %.3f"%(epoch+1, running_loss/epoch_steps))
+                     running_loss=0.0
+       
+       # Validation loss
+       net = NeuralNet(44, config['H1'], config['H2'], config['H3'], 2)
+       val_loss = 0.0
+       val_steps = 0
+       total = 0
+       correct = 0
        with torch.no_grad():
-              output = model2(input_values)
-       return output
-
-
-# %%
-
-# In[ ]:
-
-# Evaluate
-
-x_eval = torch.Tensor(X_test.dropna().to_numpy())
-# Apple yield of Australia for 2017
-print("Apple yield for 2017 \n ", Australia_yield_2017_apples["Y2017"])
-y_eval = torch.mean(torch.Tensor(Australia_yield_2017_apples["Y2017"].to_numpy()))
-y_eval = y_eval/1000000
+              val_output = net(val_subset_x.float().reshape(1,44))
+              total = val_subset_y.size(0)
+              correct = torch.sum(torch.abs(val_output-val_subset_y)<torch.abs(0.10*val_subset_y))
+              loss = criterion(val_output, val_subset_y.float())
+              val_loss += loss.cpu().numpy()
+              torch.save((net.state_dict(), optimizer.state_dict()), './trained_net.dat')
+              tune.report(loss=(val_loss), accuracy=(correct/total))
+       print("\n Finished Training \n")
+              
 #%%
-# Normalize
-x_eval_data = (x_eval-x_eval.mean())/(x_eval.max()-x_eval.min())
-x_eval_data = torch.reshape(x_eval_data,(12,1))
-# %%
-y_eval_pred = predict(x_eval_data)
-# y_eval = torch.reshape(y_eval, (12,1))
-print("ACTUAL    ",y_eval)
-print("PREDICTED   ",torch.mean(y_eval_pred))
-# %%
-# RMSE
-prediction_loss = np.sqrt(mean_squared_error(y_eval, torch.mean(y_eval_pred)))
-print("Prediction loss --> ", prediction_loss)
+# Evaluation:
+def test_accuracy(net, device='cpu'):
+       net.eval()
+       with torch.no_grad():
+              predicted = net(transformed_test_x.float().reshape(1,22))
+              return get_accuracy(net, predicted.reshape(101,1), transformed_test_y.reshape(101,1), 0.50)
+       
+# Define hyperparameters
+config = {
+       "H1":tune.sample_from(lambda _: 2**np.random.randint(5,12)),
+       "H2":tune.sample_from(lambda _: 2**np.random.randint(5,12)),
+       "H3":tune.sample_from(lambda _: 2**np.random.randint(5,12)),
+       "lr":tune.loguniform(1e-4, 1e-1)
+}
 
+# Scheduler. Randomly try out combination of hyperparameters
+scheduler = ASHAScheduler(
+       metric="loss",
+       mode="min",
+       max_t=10,
+       grace_period=1,
+       reduction_factor=2
+)
 
+reporter=CLIReporter(metric_columns=["loss","accuracy", "training_iteration"])
+
+result = tune.run(
+       partial(train_crop_yield),
+       config=config,
+       num_samples=21,
+       scheduler=scheduler
+)
+
+best_trial = result.get_best_trial("loss", "min", "last")
+
+print("Best trial config: {}".format(best_trial.config))
+print("Best trial final validation loss: {}".format(best_trial.last_result["loss"]))
+print("Best trial fnal validation accuracy: {}".format(best_trial.last_result["accuracy"]))
+
+best_trained_model = NeuralNet(22, best_trial.config["H1"], best_trial.config["H2"], best_trial.config["H3"],101)
+
+device="cpu"
+best_trained_model.to(device)
+
+# best_checkpoint_dir = best_trial.checkpoint.value
+# print(" \n",type(best_trial.evaluated_params))
+# model_state, optimizer_state = torch.load(os.path.join(best_checkpoint_dir, "checkpoint"))
+# best_trained_model.load_state_dict(model_state)
+
+num_correct, test_acc = test_accuracy(best_trained_model, device)
+print("Best trial test set accuracy: {}".format(test_acc))
+
+#%%
 # %%
